c587a177ad4b2556cd3d1ed6a3307c860e45e0f9
info <info@info09.cshl.edu>
2019-10-16 11:27:51 -0400

    add name,color,activity,animal

--- myName.py
+++ myName.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
-my_name="Alissa"
-print(my_name)
+import sys
+name = sys.argv[1]
+favorite_color = sys.argv[2]
+favorite_activity = sys.argv[3]
+favorite_animal = sys.argv[4]
+
+
+#name="Alissa"
+#print("Name: " + name)
+#favorite_color="green"
+#print("Favorite color: "+ favorite_color)
+#favorite_activity="traveling"
+#print("Favorite activity: " + favorite_activity)
+#favorite_animal="Chloe"
+#print("Favorite animal: " + favorite_animal)
+
+print("Name: ", name, "\n","Favorite color: ", favorite_color, "\n", "Favorite activity: ", favorite_activity, "\n", "Favorite animal: ", favorite_animal) 
